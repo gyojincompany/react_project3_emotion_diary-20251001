ae196b1d30105f82c3e90db4cceef2b31f77eed5
gyojincompany <luckyred@gmail.com>
2025-10-01 09:54:40 +0900

    이미지 호출 함수 util.js 추가

---
+++ src/util.js
@@ -0,0 +1,41 @@
+import emotion1 from "./img/emotion1.png"
+import emotion2 from "./img/emotion2.png"
+import emotion3 from "./img/emotion3.png"
+import emotion4 from "./img/emotion4.png"
+import emotion5 from "./img/emotion5.png"
+
+// export function getImg(emotionId) {
+//     switch (targetEmoionId) {
+//     case "1":
+//         return emotion1;       
+//     case "2":
+//         return emotion2;     
+//     case "3":
+//         return emotion3;
+//     case "4":
+//         return emotion4;         
+//     case "1":
+//         return emotion5;     
+//     default:
+//         return null;
+// }
+
+export const getEmotionImgById = (emotionId) => {
+    const targetEmoionId = String(emotionId);
+
+    switch (targetEmoionId) {
+        case "1":
+            return emotion1;       
+        case "2":
+            return emotion2;     
+        case "3":
+            return emotion3;
+        case "4":
+            return emotion4;         
+        case "1":
+            return emotion5;     
+        default:
+            return null;
+    }
+};
+
