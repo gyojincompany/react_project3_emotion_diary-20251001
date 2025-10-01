7dbc1ecd62cb4874822b385590f11cab1b278c62
gyojincompany <luckyred@gmail.com>
2025-10-01 12:12:25 +0900

    Button 공통 컴포넌트 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import { useSearchParams } from "react-router-dom";
+import Button from "../component/Button";
 
 const Home = () => {
 
-    const [searchParams, setSearchParams] = useSearchParams();
-    console.log(searchParams.get("memberid")); //request.getParameter("memberid") 유사
+    // const [searchParams, setSearchParams] = useSearchParams();
+    // console.log(searchParams.get("memberid")); //request.getParameter("memberid") 유사
 
     return (
         <div>
             <h2>Home 페이지 입니다.</h2>
+            <Button type={"positive"} text={"긍정 버튼"} onClick={()=>{alert("positive button");}} />
+            <Button type={"negative"} text={"부정 버튼"} onClick={()=>{alert("negative button");}} />
+            <Button text={"기본 버튼"} onClick={()=>{alert("default button");}} />
         </div>
     );
 }
